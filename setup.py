63edf0ca1870f428d99901cb383e68fe34d08919
Chris Johnston <chjohnston@protonmail.com>
2019-09-11 00:04:36 -0700

    update setup version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='easier68k',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/Chris-Johnston/Easier68k',
-    author='Adam Krpan, Chris Johnston, Levi Stoddard',
+    author='Easier68k Contributors',
     author_email='chjohnston@protonmail.com',
     license='MIT',
     packages=find_packages(exclude=['tests']),
