86d81bd39a992f7e231679ec6466733fa8c87b53
Chris Johnston <chjohnston@protonmail.com>
2019-09-12 22:53:23 -0700

    Update __init__.py and setup.py package metadata
    Updates metadata about the package defined in __init__.py and setup.py

--- easier68k/__init__.py
+++ easier68k/__init__.py
@@ -1,16 +1,14 @@
 """
 Easier 68k
 
-All packages associated for Easier68k
+A M68k assembler, disassembler, and simulator.
 """
 
 __title__ = 'Easier68k'
 __author__ = 'https://github.com/Chris-Johnston/Easier68k/graphs/contributors'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2018 Adam Krpan, Chris Johnston, Levi Stoddard'
-__version__ = '0.1.0'
-
-__all__ = ['simulator', 'core', 'assembler']
+__copyright__ = 'Copyright 2019 Easier68k Contributors'
+__version__ = '0.1.1'
 
 from .assembler import *
 from .assembly_parameter import *
