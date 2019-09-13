be2fc92645fbafcce9f66141d53d72b094bb87ff
Chris Johnston <chjohnston@protonmail.com>
2019-09-12 22:53:23 -0700

    Update __init__.py and setup.py package metadata
    Updates metadata about the package defined in __init__.py and setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='easier68k',
-    version='0.1.1',
-    url='https://github.com/Chris-Johnston/Easier68k',
-    author='Easier68k Contributors',
-    author_email='chjohnston@protonmail.com',
-    license='MIT',
-    packages=find_packages(exclude=['tests']),
-    setup_requires=['pytest-runner', 'pytest-cov'],
-    python_requires='>=3'
+    name="easier68k",
+    version="0.1.1",
+    url="https://github.com/Chris-Johnston/Easier68k",
+    author="Easier68k Contributors",
+    author_email="chjohnston@protonmail.com",
+    description="A M68k assembler, disassembler, and simulator.",
+    license="MIT",
+    packages=find_packages(exclude=["tests"]),
+    setup_requires=["pytest-runner", "pytest-cov"],
+    python_requires=">=3.4"
 )
 
